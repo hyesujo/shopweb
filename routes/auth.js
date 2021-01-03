1fbc5f0c14d4ed88539ca39c95d440858b2a70a2
hyesujo <63487097+hyesujo@users.noreply.github.com>
2021-01-03 18:01:51 +0900

    Add files via upload

--- routes/auth.js
+++ routes/auth.js
@@ -3,26 +3,61 @@ const router = express.Router();
 const path = require('path');
 const fs = require('fs');
 const sanitizeHtml = require('sanitize-html');
+const shortid = require('shortid');
+var ejs = require('ejs');
+let db = require('../lib/db');
+const bcrypt = require('bcrypt');
+
+const { endianness } = require('os');
+const { response } = require('express');
+const e = require('express');
 
 
+module.exports = (passport) => {
+    router.use(express.static('server'));
 
-router.use(express.static('server'));
+    router.get('/login', (req,res) => {
+        res.sendFile('login.html', { root: './server'});
+    });
+    
+    
+    router.post('/login_process', passport.authenticate('local', {
+        failureRedirect:'/auth/login'}), (req,res) => {
+            req.session.save(() => {
+                res.redirect('/logout')
+            });
+        });
 
-router.get('/login', (req,res) => {
-    res.sendFile('login.html', { root: './server'});
-});
+    router.get('/register', (req,res) => {
+        res.sendFile('register.html', {root: './server'});
+    })
 
-
-router.post('/login_process', (req, res, next) => {
-    const post = req.body;
-    const email = post.email;
-    const password = post.pwd;
-    if (email === authData.email && password === authData.password) {
-        res.send('welcome');
-    } else {
-        res.send('who?');
-    }
-});
-
-
-module.exports = router;
+    router.post('/register_process', (req, res) => {
+        let post = req.body;
+        let email = post.email;
+        let pwd = post.pwd;
+        let pwd2 = post.pwd2;
+        let displayName = post.displayName;
+        if(pwd !== pwd2) {
+            req.flash('error', 'Password must same!');
+            res.redirect('/auth/register');
+        } else {
+            bcrypt.hash(pwd, 10, (err, hash) => {
+                let user = {
+                    id: shortid.generate(),
+                    email : email,
+                    password : hash,
+                    displayName : displayName
+                }; 
+                db.get('user').push(user).write();
+                req.login(user, function(err) {
+                    console.log('redirect');
+                   req.session.save(() => {
+                    res.redirect('/logout');
+                   })
+                });
+            });
+        }
+    });
+    return router;
+}
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const fs = require('fs');
-const http = require('http');
+
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const bodyParser= require('body-parser');
+const flash = require('connect-flash');
+var ejs = require('ejs');
 
 const app = express();
-
-const authRouter = require('./routes/auth');
+var passport = require('./lib/passport')(app);
+const authRouter = require('./routes/auth')(passport);
 const compression = require('compression');
 
 app.use(express.static('server'));
@@ -23,57 +24,9 @@ app.use(session({
     store: new FileStore()
 }));
 
-const authData = {
-    email: 'poer611@naver.com',
-    password: '111111',
-    nickname: 'hs',
-};
+app.use(flash());
+app.set('view engine', "ejs");
 
-const passport = require('passport'),
-LocalStrategy = require('passport-local').Strategy;
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser(function(user,done) {
-    console.log('serializeUser', user);
-    done(null, user.email);
-});
-
-passport.deserializeUser(function(id, done) {
-     console.log('deserializeUser', id);
-   done(null,authData);
-});
-
-passport.use(new LocalStrategy(
-    {
-        usernameField: 'email',
-        passwordField: 'pwd'
-    },
-    function(username, password, done) {
-        console.log('LocalStrategy', username, password);
-        if(username === authData.email) {
-            if(password === authData.password) {
-                return done(null, authData);
-            } else {
-                return done(null, false, {
-                    message: 'Incorrect password.'
-                });
-            }
-        } else {
-          return done(null, false, {
-            message: 'Incorrect username.'
-          });   
-        }
-    }
-));
-
-app.post('/auth/login_process', passport.authenticate('local', {
-    failureRedirect:'/auth/login'}), (req,res) => {
-        req.session.save(() => {
-            res.redirect('/')
-        });
-    });
 
 app.use('/auth', authRouter);
 
@@ -81,6 +34,11 @@ app.get('/', (req,res) => {
     console.log(req.user); 
     res.sendFile('jstyle.html', { root: './server'});
 }); 
+ 
+app.get('/logout', (req,res) => {
+    req.logout();
+     res.sendFile('logout.html', {root: './server'});
+});
 
 
 app.use((req,res,next) => {
